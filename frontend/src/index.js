4cb70a02ae572526f31920fa68c2cba42cc2fd58
chengtie <chengtie@gmail.com>
2023-12-15 16:24:17 +0800

    trying to make add-in

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Check if Office is in the context (indicating that the add-in is running in Excel)
+if (typeof Office !== 'undefined' && Office.onReady) {
+  Office.onReady((info) => {
+    if (info.host === Office.HostType.Excel) {
+      // Office is ready, render your React app
+      ReactDOM.render(
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>,
+        document.getElementById('root')
+      );
+    }
+  });
+} else {
+  // Office is not available (e.g., when running outside of Excel), you can handle this case accordingly
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
