d8ee891fd7bd90098434311ece9ece151980c7d3
chengtie <chengtie@gmail.com>
2023-12-15 20:26:57 +0800

    removed Office.js from frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,27 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// Check if Office is defined and Office.onReady is a function
-if (typeof Office !== 'undefined' && typeof Office.onReady === 'function') {
-  Office.onReady((info) => {
-    if (info.host === Office.HostType.Excel) {
-      // Office is ready, render your React app
-      ReactDOM.render(
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>,
-        document.getElementById('root')
-      );
-    }
-  });
-} else {
-  // Office is not available (e.g., when running outside of Excel), you can handle this case accordingly
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
